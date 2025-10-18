5772ba01a404780827586ee99775c71d32a25fd1
Rafee <rafeehasan88@gmail.com>
2025-10-18 12:56:00 +0600

    delete operation added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 app =express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Middleware
 app.use(cors());
@@ -36,7 +36,7 @@ async function run() {
       const cursor = coffeeCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
 
     app.post('/coffee' , async(req , res)=>{
@@ -44,7 +44,15 @@ async function run() {
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
-    })
+    });
+
+    app.delete('/coffee/:id' , async(req , res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+
+    });
 
 
 
